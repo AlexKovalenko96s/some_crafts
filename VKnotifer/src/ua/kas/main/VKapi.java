53d7b4a097a575503d00c80ec41fee1fdc30e630
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-12-18 21:07:20 +0200

    ..

--- VKnotifer/src/ua/kas/main/VKapi.java
+++ VKnotifer/src/ua/kas/main/VKapi.java
@@ -25,8 +25,8 @@ public class VKapi {
 	private String display = "popup";
 	private String response_type = "token";
 	private String access_token;
-	private String email = "******";// тут должен быть прописан email
-	private String pass = "******";// тут должен быть прописан пароль
+	private String email = "******";
+	private String pass = "******";
 
 	public void setConnection() throws IOException, URISyntaxException {
 		HttpClient httpClient = new DefaultHttpClient();
