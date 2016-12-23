58142dde566c52043e736dc200c9c5c9dd58638c
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-12-23 13:17:29 +0200

    //

--- VKnotifer/src/ua/kas/main/VKapi.java
+++ VKnotifer/src/ua/kas/main/VKapi.java
@@ -4,18 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.params.ClientPNames;
-import org.apache.http.client.params.CookiePolicy;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 public class VKapi {
 
@@ -29,44 +22,53 @@ public class VKapi {
 	private String pass = "******";
 
 	public void setConnection() throws IOException, URISyntaxException {
-		HttpClient httpClient = new DefaultHttpClient();
-		httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.BROWSER_COMPATIBILITY);
-		// Делаем первый запрос
-		HttpPost post = new HttpPost("http://oauth.vk.com/authorize?" + "client_id=" + client_id + "&scope=" + scope
-				+ "&redirect_uri=" + redirect_uri + "&display=" + display + "&response_type=" + response_type);
-		HttpResponse response;
-		response = httpClient.execute(post);
-		post.abort();
-		// Получаем редирект
-		System.out.println(response.toString());
-		String HeaderLocation = response.getFirstHeader("location").getValue();
-		URI RedirectUri = new URI(HeaderLocation);
-		// Для запроса авторизации необходимо два параметра полученных в первом
-		// запросе
-		// ip_h и to_h
-		String ip_h = RedirectUri.getQuery().split("&")[2].split("=")[1];
-		String to_h = RedirectUri.getQuery().split("&")[4].split("=")[1];
-		// Делаем запрос авторизации
-		post = new HttpPost("https://login.vk.com/?act=login&soft=1" + "&q=1" + "&ip_h=" + ip_h
-				+ "&from_host=oauth.vk.com" + "&to=" + to_h + "&expire=0" + "&email=" + email + "&pass=" + pass);
-		response = httpClient.execute(post);
-		post.abort();
-		// Получили редирект на подтверждение требований приложения
-		HeaderLocation = response.getFirstHeader("location").getValue();
-		post = new HttpPost(HeaderLocation);
-		// Проходим по нему
-		response = httpClient.execute(post);
-		post.abort();
-		// Теперь последний редирект на получение токена
-		HeaderLocation = response.getFirstHeader("location").getValue();
-		// Проходим по нему
-		post = new HttpPost(HeaderLocation);
-		response = httpClient.execute(post);
-		post.abort();
-		// Теперь в след редиректе необходимый токен
-		HeaderLocation = response.getFirstHeader("location").getValue();
-		// Просто спарсим его сплитами
-		access_token = HeaderLocation.split("#")[1].split("&")[0].split("=")[1];
+		// HttpClient httpClient = new DefaultHttpClient();
+		// httpClient.getParams().setParameter(ClientPNames.COOKIE_POLICY,
+		// CookiePolicy.BROWSER_COMPATIBILITY);
+		// // Делаем первый запрос
+		// HttpPost post = new HttpPost("http://oauth.vk.com/authorize?" +
+		// "client_id=" + client_id + "&scope=" + scope
+		// + "&redirect_uri=" + redirect_uri + "&display=" + display +
+		// "&response_type=" + response_type);
+		// HttpResponse response;
+		// response = httpClient.execute(post);
+		// post.abort();
+		// // Получаем редирект
+		// System.out.println(response.toString());
+		// String HeaderLocation =
+		// response.getFirstHeader("location").getValue();
+		// URI RedirectUri = new URI(HeaderLocation);
+		// // Для запроса авторизации необходимо два параметра полученных в
+		// первом
+		// // запросе
+		// // ip_h и to_h
+		// String ip_h = RedirectUri.getQuery().split("&")[2].split("=")[1];
+		// String to_h = RedirectUri.getQuery().split("&")[4].split("=")[1];
+		// // Делаем запрос авторизации
+		// post = new HttpPost("https://login.vk.com/?act=login&soft=1" + "&q=1"
+		// + "&ip_h=" + ip_h
+		// + "&from_host=oauth.vk.com" + "&to=" + to_h + "&expire=0" + "&email="
+		// + email + "&pass=" + pass);
+		// response = httpClient.execute(post);
+		// post.abort();
+		// // Получили редирект на подтверждение требований приложения
+		// HeaderLocation = response.getFirstHeader("location").getValue();
+		// post = new HttpPost(HeaderLocation);
+		// // Проходим по нему
+		// response = httpClient.execute(post);
+		// post.abort();
+		// // Теперь последний редирект на получение токена
+		// HeaderLocation = response.getFirstHeader("location").getValue();
+		// // Проходим по нему
+		// post = new HttpPost(HeaderLocation);
+		// response = httpClient.execute(post);
+		// post.abort();
+		// // Теперь в след редиректе необходимый токен
+		// HeaderLocation = response.getFirstHeader("location").getValue();
+		// // Просто спарсим его сплитами
+		// access_token =
+		// HeaderLocation.split("#")[1].split("&")[0].split("=")[1];
+		access_token = "e10f5aa439430959976894ff81bc128fe90df1bdfbbede4b64134b404ffa8cb6183cfc81a457d38db15cb";
 	}
 
 	public String getNewMessage()
