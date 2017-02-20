9e224eb708fd6bf75dd3f248fc272bbaae9c38da
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2017-02-20 18:51:23 +0200

    //

--- SQLite_Test/src/ua/kas/main/SqliteConnection.java
+++ SQLite_Test/src/ua/kas/main/SqliteConnection.java
@@ -8,7 +8,7 @@ public class SqliteConnection {
 	public static Connection Connector() {
 		try {
 			Class.forName("org.sqlite.JDBC");
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:EmployeeDB.sqlite");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:res/EmployeeDB.sqlite");
 			return conn;
 		} catch (Exception e) {
 			System.out.println(e);
