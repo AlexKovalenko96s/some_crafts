5479e3dfe28c44f756d964e00b0c360a012b03db
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2017-08-11 21:12:41 +0300

    //

---
+++ Task_First/src/ua/kas/main/First.java
@@ -0,0 +1,27 @@
+package ua.kas.main;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/First")
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+
+		PrintWriter pw = response.getWriter();
+
+		pw.println("привет мир");
+		pw.close();
+	}
+}
